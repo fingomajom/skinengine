074e19a5bd3e1b3097254bb80688fd5ffc501954
wgkujgg <wgkujgg@163.com>
2007-12-17 09:13:15 +0000

    打标签

--- publish/skinapp.h
+++ publish/skinapp.h
@@ -1,4 +1,13 @@
-
+/********************************************************************
+* CreatedOn: 2007-12-17   17:07
+* FileName: skinapp.h
+* CreatedBy: lidengwang <lidengwang@kingsoft.net>
+* $LastChangedDate$
+* $LastChangedRevision$
+* $LastChangedBy$
+* $HeadURL:  $
+* Purpose:
+*********************************************************************/
 
 #pragma once
 
