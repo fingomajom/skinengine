1a8eb421d4d7acf3e5f4163dfd028f0d220bf417
wgkujgg@163.com <wgkujgg@163.com@060dfaac-7641-0410-8d08-bb87ba2ae35e>
2008-05-04 07:36:23 +0000

    change

--- kis/trunk/src/kissvr/_ISvrObjectEvents_CP.h
+++ kis/trunk/src/kissvr/_ISvrObjectEvents_CP.h
@@ -1,8 +1,21 @@
+/********************************************************************
+* CreatedOn: 2008-5-4   14:11
+* FileName: _ISvrObjectEvents_CP.h
+* CreatedBy: lidengwang <lidengwang@kingsoft.net>
+* $LastChangedDate$
+* $LastChangedRevision$
+* $LastChangedBy$
+* $HeadURL:  $
+* Purpose:
+*********************************************************************/
+
 #pragma once
+
+#include "ATLCPImplMT.h"
 
 template<class T>
 class CProxy_ISvrObjectEvents :
-	public IConnectionPointImpl<T, &__uuidof(IDispatch)>
+	public IConnectionPointImplMT<T, &__uuidof(IDispatch), CComDynamicUnkArray>
 {
 public:
 	HRESULT Fire_NotifyMessage( ULONG uDesCallerId,  ULONG uSrcModuleId,  BSTR bstrFunctionName,  BSTR bstrParameter,  BSTR * bstrResult)
